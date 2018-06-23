4c63181fd37444ca8aeaf5ee01a2cfee28c8ea9f
David Zhu <david@alluxio.com>
2018-06-22 18:30:24 -0700

    disable locking

--- core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
@@ -786,11 +786,13 @@ public class InodeTree implements JournalEntryIterable {
       Inode<?> childInode, String[] pathComponents) throws FileDoesNotExistException, InvalidPathException {
     InodeLockList inodeLockList = new InodeLockList();
 
+    /*
     if (lockMode == LockMode.READ) {
       inodeLockList.lockReadAndCheckParent(childInode, inodePath.getInode());
     } else {
       inodeLockList.lockWriteAndCheckParent(childInode, inodePath.getInode());
-    }
+    } */
+
     if (pathComponents == null) {
       return new MutableLockedInodePath(inodePath.getUri().join(childInode.getName()),
           new CompositeInodeLockList(inodePath.mLockList, inodeLockList), lockMode);
