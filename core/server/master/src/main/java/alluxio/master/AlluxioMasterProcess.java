0857d7cad9ecfbb8c59a0781f7911d7ca5a96254
David Zhu <david@alluxio.com>
2019-02-20 15:15:55 -0800

    go back to fixedthreadpool

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -40,6 +40,7 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
@@ -281,7 +282,7 @@ public class AlluxioMasterProcess extends MasterProcess {
           ServerConfiguration.global());
 
       mRPCExecutor =
-          new ForkJoinPool(ServerConfiguration.getInt(
+          Executors.newFixedThreadPool(ServerConfiguration.getInt(
               PropertyKey.MASTER_RPC_FORKJOIN_POOL_PARALLELISM));
       serverBuilder.executor(mRPCExecutor);
       for (Master master : mRegistry.getServers()) {
